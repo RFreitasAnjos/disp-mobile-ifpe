d0bdb55a9809522e462434d2e1ff832e23ae30b4
Renan Freitas <renan.freitas.anjos@gmail.com>
2025-05-27 18:07:25 -0300

    Create exercicio08 with dir mobile and backend

---
+++ exercicio07/backend/Index.js
@@ -0,0 +1,54 @@
+const cloudinary = require('cloudinary');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+
+cloudinary.config({ 
+    cloud_name: 'doe9vhxyb', 
+    api_key: '848875746746698', 
+    api_secret: '<your_api_secret>' // Click 'View API Keys' above to copy your API secret
+});
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/imagens', async(req,res) => {
+    const { tag = 'aula08ifpe'} = req.query;
+    
+    try {
+        const result = await cloudinary.api.resources_by_tag(tag,{
+            type: 'upload',
+            prefix: '',
+            max_results: 100,
+        });
+        res.json(result.resources);
+    } catch ( err ) {
+        console.error("Error:", err);
+        res.status(500).json({ error: 'Erro ao buscar imagens' });
+    }
+});
+
+app.delete('/delete-image', async(req,res) => {
+    if(!public_id){
+        return res.status(400).json({ error: 'public_id é obrigatório.'});
+    }
+
+    try{
+        const result = await cloudinary.uploader.destrou(public_id);
+        res.json(result);
+    } catch ( err ) {
+        console.error(err);
+        res.status(500).json({ error: 'Erro ao deleter imagem.'});
+    }
+});
+
+app.listen(PORT, () => {
+    try{
+        console.log(`Servidor rodando em http://10.31.88.99:${PORT}`)
+    } catch ( err ) {
+        console.error("Erro: ", err)
+    }
+});
